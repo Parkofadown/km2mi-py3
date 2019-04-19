d941b6802d87a321ebfb90d17505de2b338291de
Richard <38399137+Parkofadown@users.noreply.github.com>
2019-04-19 19:53:28 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+
+print("Enter number of kilometers: ")
+kms = input()
+miles = float(kms) / 1.60934
+rounded_miles = round(miles,2)
+print(f"Your {kms} km distance is equal to {rounded_miles} mi")
+ 
